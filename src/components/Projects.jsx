aa6ca8ad410d2490bf64add495333adc90312d3b
Vishwashankar T. Janakiraman <38104352+vishwashankartj@users.noreply.github.com>
2025-11-30 23:21:29 -0500

    feat: Add gh-pages for deployment and refine UI/UX across Hero, Navbar, Tech, and About sections with updated content.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,9 +20,8 @@ const ProjectCard = ({
   return (
     <motion.div
       variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-      className={`relative ${
-        active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
-      } flex items-center justify-center min-w-[170px] 
+      className={`relative ${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
+        } flex items-center justify-center min-w-[170px] 
       h-[420px] cursor-pointer card-shadow`}
       onClick={() => handleClick(id)}>
       <div
