534912d324831ee406e63f0f3aa8d923dd62bc7b
Vishwashankar T. Janakiraman <38104352+vishwashankartj@users.noreply.github.com>
2025-12-01 00:16:26 -0500

    feat: Configure app for GitHub Pages deployment with base URL and add Google site verification.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/vishwashankarTJanakiraman.github.io">
       <div className="relative z-0">
         <div>
           <Navbar />
